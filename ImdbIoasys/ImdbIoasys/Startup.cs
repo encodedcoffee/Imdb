94b6b11a79c1e29d2c5c0969098639398e40f01a
Eduardo Carvalho <eduardosam07@gmail.com>
2021-02-02 14:29:33 -0300

    Criação do controller de usuários e injeção de dependencia do contexto

--- ImdbIoasys/ImdbIoasys/Startup.cs
+++ ImdbIoasys/ImdbIoasys/Startup.cs
@@ -1,15 +1,10 @@
+using ImdbIoasys.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ImdbIoasys
 {
@@ -27,6 +22,7 @@ namespace ImdbIoasys
         {
 
             services.AddControllers();
+            services.AddDbContext<ImdbContext>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ImdbIoasys", Version = "v1" });
