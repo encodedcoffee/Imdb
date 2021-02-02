9fee6c101271904d7d716386227d02e9356892bd
Eduardo Carvalho <eduardosam07@gmail.com>
2021-02-02 13:35:11 -0300

    Adicionando pacotes do entity framework core, criação do contexto de BD e Inicializando 'migration' de criação da tabela de usuários

---
+++ ImdbIoasys/ImdbIoasys/Data/ImdbContext.cs
@@ -0,0 +1,15 @@
+﻿using ImdbIoasys.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImdbIoasys.Data
+{
+    public class ImdbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=ImdbIoasys;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ ImdbIoasys/ImdbIoasys/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ImdbIoasys.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+        public bool Administrador { get; set; }
+    }
+}
