c6c1f88aa82a051f31a00626531a409dba14314a
Eduardo Carvalho <eduardosam07@gmail.com>
2021-03-02 09:14:33 -0300

    Refatorando referencias

---
+++ Imdb/ImdbIoasys/Startup.cs
@@ -0,0 +1,99 @@
+using Application.Interfaces.Services;
+using Application.Services;
+using Infrastructure.DbConfiguration.EfCore;
+using Infrastructure.Interfaces.Repositories;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Imdb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<DbContext, ImdbContext>();
+            RegistrarRepositoriosEServicos(services);
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.EnableAnnotations();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Imdb", Version = "v1" });
+            });
+
+            ConfigurarAutenticacao(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Imdb v1"));
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void RegistrarRepositoriosEServicos(IServiceCollection services)
+        {
+            #region Repositorios
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IFilmeRepository, FilmeRepository>();
+            #endregion
+
+            #region Serviços
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<IFilmeService, FilmeService>();
+            #endregion
+        }
+
+        private void ConfigurarAutenticacao(IServiceCollection services)
+        {
+            var chaveAutenticacao = "kvjr5yGd0b^sO*olDs0CbbBzJp8mBa";//TODO definir no arquivo JSON de configuração
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(chaveAutenticacao)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
